e9eb66c83931c7894a61b5fa2edb153c0836e4d1
AlexASC21 <87743123+Alex81405@users.noreply.github.com>
2023-06-07 23:20:52 -0400

    Update index.js

--- sql-project/src/router/index.js
+++ sql-project/src/router/index.js
@@ -1,23 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import CreateView from "../views/CreateView.vue";
+import storePage from "../views/storePage.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/create",
+      name: "create",
+      component: CreateView,
+    },
+    {
+      path: "/store",
+      name: "store",
+      component: storePage
     }
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
