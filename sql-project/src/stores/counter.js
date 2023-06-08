1f6f410fbd90298efce06020ecb030f961785699
AlexASC21 <87743123+Alex81405@users.noreply.github.com>
2023-06-07 23:11:01 -0400

    Revert "Update counter.js"
    This reverts commit b8b7a6c7aa9b8f08e2db509c19538790e57c2a03.

--- sql-project/src/stores/counter.js
+++ sql-project/src/stores/counter.js
@@ -2,10 +2,11 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
-  let page = 'homepage'
-  let loggedIn = false
-  let email = ''
-  let password = ''
-  let id = ''
-  return { page, email, password, id, loggedIn,}
+  const count = ref(0)
+  const doubleCount = computed(() => count.value * 2)
+  function increment() {
+    count.value++
+  }
+
+  return { count, doubleCount, increment }
 })
