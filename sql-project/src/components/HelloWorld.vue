9932b548558422946bcfc45415aac414a2050edd
AlexASC21 <87743123+Alex81405@users.noreply.github.com>
2023-06-07 23:18:32 -0400

    Update HelloWorld.vue

--- sql-project/src/components/HelloWorld.vue
+++ sql-project/src/components/HelloWorld.vue
@@ -2,18 +2,16 @@
 defineProps({
   msg: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      buy our books buy our books buy our books buy our books buy our books buy our books play tears of the kingdom buy our books buy our books buy our books buy our books buy our books buy our books WHY IS MY WEBSITE OFF-CENTER??? buy our books buy our books buy our books buy our books buy our books buy our books buy our books buy our books 
     </h3>
   </div>
 </template>
@@ -23,11 +21,25 @@ h1 {
   font-weight: 500;
   font-size: 2.6rem;
   top: -10px;
+  margin-top: 40px;
 }
 
 h3 {
   font-size: 1.2rem;
 }
+
+.greetings {
+  height: 10em;
+  position: relative }
+.greetings p {
+  margin: 0;
+  background: yellow;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%) }
+
 
 .greetings h1,
 .greetings h3 {
@@ -37,7 +49,7 @@ h3 {
 @media (min-width: 1024px) {
   .greetings h1,
   .greetings h3 {
-    text-align: left;
+    text-align: center;
   }
 }
 </style>
