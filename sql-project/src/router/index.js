d56d84f060782c0cc920c65c76e2f8f791307081
AlexASC21 <87743123+Alex81405@users.noreply.github.com>
2023-06-07 23:00:12 -0400

    Update index.js

--- sql-project/src/router/index.js
+++ sql-project/src/router/index.js
@@ -1,23 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useCounterStore } from '../stores/counter'
+import loggedinpage from '../views/loggedInHomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/signin',
+      name: '/signin',
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/loggedinpage',
+      name: 'home',
+      component: loggedinpage
     }
+
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/blogpage.vue')
+    // }
   ]
 })
+
+
 
 export default router
